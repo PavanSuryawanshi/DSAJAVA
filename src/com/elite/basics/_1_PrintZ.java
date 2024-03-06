d45b68ce15679ef56fc3deb3701ede349c32a73c
Pavan <pavansuryawanshi31@gmail.com>
2024-03-06 12:00:07 +0530

    updated

--- src/com/elite/basics/_1_PrintZ.java
+++ src/com/elite/basics/_1_PrintZ.java
@@ -3,5 +3,6 @@ package com.elite.basics;
 public class _1_PrintZ {
     public static void main(String[] args) {
         System.out.println("Hello guys");
+        System.out.println("Hello Girls");
     }
 }
